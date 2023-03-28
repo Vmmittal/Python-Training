7d222e92e6dbecce848d1c47a0735198971fd098
Vipul Kumar <103205065+Vmmittal@users.noreply.github.com>
2023-03-28 16:24:08 +0530

    (TypeCasting, Set,Tuple, Dict,list) Example

---
+++ class1.py
@@ -0,0 +1,54 @@
+print("hello")
+
+'''Type Casting'''
+a=1232
+print(a)
+
+b=123.32
+print(b)
+
+c="1234"
+print(c)
+
+casting=float(a)
+print(casting)
+
+casting2=int(b)
+print(casting2)
+
+casting3=str(a)
+print(casting3)
+
+'''List Example'''
+list=[1,2,3,4,3.4,"vipul"]
+print(list)
+
+print(list[5])
+print(list[-3])
+list[-1]="vm"
+print(list)
+
+'''Tuple Example'''
+tuple=(2,3,2,4,55,66.8,76,'vipul')
+print(tuple)
+print(tuple[5])
+
+'''Dictionary'''
+dict={
+    "name":"vipul",
+    "emp id":592,
+    
+}
+print(dict)
+print(dict["name"])
+
+print(type(c))
+
+str2='vipulmitt'
+print(type(str2))
+
+#set Example
+set={1,'vm00',3,.43,400,400}
+print(set)
+set.add("cloudeq")
+print(set)
