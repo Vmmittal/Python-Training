594f765bec90c0fc895b197e317cb5d2a9bcd187
Vipul Kumar <103205065+Vmmittal@users.noreply.github.com>
2023-04-12 19:42:44 +0530

    Sprint-3 Class_1(Rest API's)

--- class1.py
+++ class1.py
@@ -1,60 +1,57 @@
-print("hello")
 
-'''Type Casting'''
-a=1232
-print(a)
+"""Stateless nature of rest API's"""
 
-b=123.32
-print(b)
+#A Stateless Protocol is a type of network protocol in which a server responds to the client requests based on the current state
 
-c="1234"
-print(c)
+"""Status Codes
+503 : typically indicates a performance issue on the origin server. 
 
-casting=float(a)
-print(casting)
+403 :Forbidden Error,indicates that the server understands the request but refuses to authorize it.
 
-casting2=int(b)
-print(casting2)
+301:  the requested resource has been definitively moved to the URL given by the Location headers."""
 
-casting3=str(a)
-print(casting3)
 
-'''List Example'''
-list=[1,2,3,4,3.4,"vipul"]
-print(list)
 
-print(list[5])
-print(list[-3])
-list[-1]="vm"
-print(list)
-list.append("cloudeq")
-print(list)
-list.insert(2,"python")
-print(list)
-list.pop(2)
-print(list)
+"""C  R  U   D   Operations"""
 
-'''Tuple Example'''
-tuple=(2,3,2,4,55,66.8,76,'vipul')
-print(tuple)
-print(tuple[5])
+import requests
 
-'''Dictionary'''
-dict={
-    "name":"vipul",
-    "emp id":592,
-    
-}
-print(dict)
-print(dict["name"])
 
-print(type(c))
+# POST method 
+my_url="https://fakestoreapi.com/products"
+my_data={"user_id":1112,"title":"Hello"}
+response1=requests.post(my_url,json=my_data)
+print(response1)
+print(response1.json())
 
-str2='vipulmitt'
-print(type(str2))
 
-#set Example
-set={1,'vm00',3,.43,400,400}
-print(set)
-set.add("cloudeq")
-print(set)
+#GET method
+my_url="https://jsonplaceholder.typicode.com/todos/1#"
+response2=requests.get(my_url)
+print(response2)
+print(response2.json())
+
+
+#PUT method
+my_url="https://jsonplaceholder.typicode.com/todos/1#"
+my_data2={"user_id":1134,"title":"Hello"}
+response3=requests.put(my_url,json=my_data2)
+print(response3)
+# print(response3.json())
+
+
+#PATCH method
+my_url="https://fakestoreapi.com/products/3"
+my_data3={"title":"Hello-Vipul"}
+response4=requests.patch(my_url,json=my_data3)
+# response4=requests.get(my_url)
+
+print(response4)
+print(response4.json())
+
+
+# #DELETE method
+my_url2="https://reqres.in/api/users/2"
+response5=requests.delete(my_url2)
+print(response5)
+
